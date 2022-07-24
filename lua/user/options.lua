769a9837255896ac6fab092e293974c7c81f4877
yipinliu <ypliu18@gmail.com>
2022-07-24 14:07:54 +0800

    Add cmp

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -34,7 +34,7 @@ local options = {
     wrap = false,                               -- display lines as one long line
     scrolloff = 8,                              -- is one of my fav
     sidescrolloff = 8,
-    guifont = "cascadia code pl:h17",           -- the font used in graphical neovim applications
+    guifont = "CaskaydiaCove NF:h17",           -- the font used in graphical neovim applications
 }
 
 vim.opt.shortmess:append "c"
