41769daab26aea70bbd10d181a7be079c7f504de
yipinliu <ypliu18@gmail.com>
2022-07-24 17:22:33 +0800

    Fixed mini issues

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -10,9 +10,9 @@ end
 
 require("luasnip/loaders/from_vscode").lazy_load()
 
-local check_backspace = function()
-    local col = vim.fn.col "." -1
-    return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+local has_word_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 --   פּ ﯟ   some other good icons
 local kind_icons = {
@@ -71,8 +71,8 @@ cmp.setup {
                 luasnip.expand()
             elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
-            elseif check_backspace() then
-                fallback()
+            elseif has_word_before() then
+                cmp.complete()
             else
                 fallback()
             end
